9ba4cec74bf4152b61cf6da477ca15f1c579b285
Walter <walterlaicollege@gmail.com>
2023-01-19 17:23:45 -0500

    changed variable names and used reshape instead of newaxis

--- 000_simplest_md.py
+++ 000_simplest_md.py
@@ -19,12 +19,17 @@ def force(positions):
     For each atom, calculate the forces that other atoms exert on it on the XYZ directions
     '''
     #https://stackoverflow.com/questions/25965329/difference-between-every-pair-of-columns-of-two-numpy-arrays-how-to-do-it-more
-    per_dimension_distances = positions[:,np.newaxis,:] - positions #this line was taken from stack overflow
-    squared_cartesian_distances = np.sum(per_dimension_distances**2,axis=2)
-    gradients = gradient_return(squared_cartesian_distances,1,1)
-    interatomic_forces_per_atom = gradients[:,:,np.newaxis] * per_dimension_distances
-    interatomic_forces_per_atom[np.isnan(interatomic_forces_per_atom)] = 0
-    new_forces = np.sum(interatomic_forces_per_atom,axis=0)
+    n_atoms, n_dimensions            = positions.shape
+    each_atom_coordinates            = positions.reshape(n_atoms, 1, n_dimensions)#from stack overflow
+    interatom_distances_per_dimension_per_atom = each_atom_coordinates - positions
+
+    squared_cartesian_distances  = np.sum(interatom_distances_per_dimension_per_atom**2,axis=2)
+    gradients_between_atoms      = gradient_return(squared_cartesian_distances, 1, 1)
+    interatom_gradients_per_atom = gradients_between_atoms.reshape(n_atoms, n_atoms, 1)
+
+    force_per_dimension_per_atom = interatom_gradients_per_atom * interatom_distances_per_dimension_per_atom
+    force_per_dimension_per_atom[np.isnan(force_per_dimension_per_atom)] = 0
+    new_forces = np.sum(force_per_dimension_per_atom,axis=0)
     return new_forces
 
 ndim        = 3
@@ -40,11 +45,10 @@ positions[0,:] = [0.5391356726,0.1106588251,-0.4635601962] #atom1
 positions[1,:] = [-0.5185079933,0.4850176090,0.0537084789] #atom2
 positions[2,:] = [0.0793723207,-0.4956764341,0.5098517173] #atom3
 
+forces     =  force(positions)
 velocities = np.zeros(shape=(natm,ndim))
-forces     = np.zeros(shape=(natm,ndim))
 potentials = np.zeros(shape=(natm,natm))
 
-forces =  force(positions)
 
 for step in range(nsteps):
     velocities += forces * timestep / 2 / masses #take a halfstep forward and calculate new positions
